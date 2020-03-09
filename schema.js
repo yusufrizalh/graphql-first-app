a73da7af166b9ccca26cbfdbe2f3470fdac152b5
Yusuf Rizal <yusufrizalh@gmail.com>
2020-03-09 16:29:01 +0700

    add Peoples via axios

--- schema.js
+++ schema.js
@@ -7,16 +7,19 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+const axios = require('axios');
+
+/*
 // Dummy data
 const peoples = [
     {id: '1', nama: 'Yusuf Rizal', email: 'yrizal@email', usia: 28}, 
     {id: '2', nama: 'Farah Diba', email: 'fdiba@email', usia: 30}, 
     {id: '3', nama: 'Benazir Shea', email: 'bshea@email', usia: 1}, 
-]; 
+]; */  
 
 // Peoples Type
 const PeoplesType = new GraphQLObjectType({
-    name: 'peoples', 
+    name: 'Peoples', 
     fields: () => ({
         id: {type: GraphQLString}, 
         nama: {type: GraphQLString}, 
@@ -29,17 +32,27 @@ const PeoplesType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType', 
     fields: {
-        people: {
+        People: {
             type: PeoplesType, 
             args: {
                 id: {type: GraphQLString}
             }, 
             resolve(parentValue, args) {
+                /*
                 for(let i = 0; i < peoples.length; i++) {
                     if(peoples[i].id == args.id) {
                         return peoples[i]; 
                     }
-                }
+                }*/ 
+                return axios.get('http://localhost:3000/peoples/'+args.id)
+                    .then(res => res.data);
+            }
+        }, 
+        Peoples: {
+            type: new GraphQLList(PeoplesType), 
+            resolve(parentValue, args) {
+                return axios.get('http://localhost:3000/peoples')
+                    .then(res => res.data);
             }
         }
     }
